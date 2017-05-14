cd615061f5c5c8a628de3e2cac4e3a7f39acd680
Metin <metinsn@outlook.com>
2017-05-14 20:40:58 +0300

    Yavuz Hocanın Örneği 14052017

---
+++ MVC_Ajax/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name, string pass)
+        {
+            if (name == "yavuz" && pass == "1234")
+            {
+                return Json(new { success = true, mesaj = "Giriş Başarılı" });
+            }
+            else
+            {
+                return Json(new { success = false, mesaj = "Hatalı Giriş" });
+            }
+        }
+    }
+}
